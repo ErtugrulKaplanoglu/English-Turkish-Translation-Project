0b2dd9e988b4871223f75743a8fa04e7ca207145
ErtugrulKaplanoglu <ertugrul_kaplanoglu_25@outlook.com>
2025-12-25 00:33:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,27 @@ tr2en_engine = Translator("models/final_tr2en_model.pt", "models/tokenizer_bpe.j
 
 while True:
     source = input('Cümlenizi Giriniz / Enter Your Sentence: ')
-    
-    if source == 'exit_':
-        break
-    
+        
     lang = util.detect_language(source)
     
-    #dil tespitine göre çevirici modellere çağrı yapılan kısım
     if lang == 'eng':
-    
-        #print(en2tr_translate(source))
-        print(en2tr_engine.translate(source))
-        print(translate_en2tr(source))
+        
+        print(en2tr_engine.translate(source) + '\t(translated by Transformers Model.)')
+        print(translate_en2tr(source) + '\t(translated by Seq2Seq Model.)')
         print(marianNMT_en2tr(source) + '\t(translated by MarianNMT.)')
         pass
        
     elif lang == 'tur':
-        #print(en2tr_translate(source))
-        print(tr2en_engine.translate(source))
+        
+        print(tr2en_engine.translate(source) + '\t(Transformer Model  ile çevrildi.)')
+        print(translate_tr2en(source) + '\t(Seq2Seq Model ile çevrildi.)')
         print(marianNMT_tr2en(source) + '\t(MarianNMT ile çevrildi.)')
         pass
     
     elif lang == 'unk':
         print('Unknown language!!!')
-   
-
-    
-
+        
+    cont = input('\n Devam edilsin mi/is Continued? (y/n)').lower()
+    if cont == 'n':
+        break
 
